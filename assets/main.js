059f6799340e22f8d03c5ef1c5e191f92e326e06
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-07-24 12:49:27 -0700

    Make it work if multiple shortcodes on the same page.

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,35 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const link = document.querySelector('.rs-link');
-  if (!link) {
+  const links = document.querySelectorAll('.rs-link');
+  if (!links) {
       return;
   }
 
-  const assetName = link.dataset.assetName;
-  const tooltip = document.createElement('div');
-  tooltip.style.display = 'none';
-  tooltip.style.position = 'absolute';
-  tooltip.style.background = 'white';
-  tooltip.style.border = '1px solid black';
-  tooltip.style.padding = '10px';
-  link.parentElement.appendChild(tooltip);
-
-  link.addEventListener('mouseover', (event) => {
-      fetch('https://data.rarepepes.com/items/nft')
-          .then(response => response.json())
-          .then(data => {
-              const assetData = data.data.find(item => item.asset_name === assetName);
-              if (!assetData) {
-                  tooltip.textContent = 'No data found for this asset.';
-              } else {
-                  tooltip.textContent = `Issuer: ${assetData.issuer}, Quantity: ${assetData.quantity}`;
-              }
-              tooltip.style.display = 'block';
-          });
-  });
-
-  link.addEventListener('mouseout', (event) => {
+  links.forEach((link) => {
+      const assetName = link.dataset.assetName;
+      const tooltip = document.createElement('div');
       tooltip.style.display = 'none';
+      tooltip.style.position = 'absolute';
+      tooltip.style.background = 'white';
+      tooltip.style.border = '1px solid black';
+      tooltip.style.padding = '10px';
+      link.parentElement.appendChild(tooltip);
+
+      link.addEventListener('mouseover', (event) => {
+          fetch('https://data.rarepepes.com/items/nft')
+              .then(response => response.json())
+              .then(data => {
+                  const assetData = data.data.find(item => item.asset_name === assetName);
+                  if (!assetData) {
+                      tooltip.textContent = 'No data found for this asset.';
+                  } else {
+                      tooltip.textContent = `Issuer: ${assetData.issuer}, Quantity: ${assetData.quantity}`;
+                  }
+                  tooltip.style.display = 'block';
+              });
+      });
+
+      link.addEventListener('mouseout', (event) => {
+          tooltip.style.display = 'none';
+      });
   });
 });
