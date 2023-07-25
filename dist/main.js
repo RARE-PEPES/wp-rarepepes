da52a67eb6e09b098e7a58ec3efdbeb8e2823d7c
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-07-25 13:28:31 -0700

    Refactor and clean up so it's easier to read.

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ let assetNamesOnPage = rsData.assetNames; // Defined in shortcode.php
 let activeTooltip = null;
 let documentTouchTriggered = false;
 
+// Fetch data from server
 const fetchData = async () => {
     try {
         const response = await fetch('https://data.rarepepes.com/items/nft');
@@ -14,6 +15,7 @@ const fetchData = async () => {
     }
 }
 
+// Preload images from fetched data
 const preloadImages = () => {
     nftData.forEach(item => {
         if (assetNamesOnPage.includes(item.asset_name)) {
@@ -24,30 +26,42 @@ const preloadImages = () => {
     });
 }
 
+// Generate tooltip HTML
+const generateTooltipHTML = (assetData) => {
+    if (!assetData) {
+        return 'No data found for this asset.';
+    } else {
+        return `
+            <h3>${assetData.asset_name}</h3>
+            <p>Series <strong>${assetData.series}</strong>&nbsp; Card <strong>${assetData.order}</strong></p>
+            <img src="${assetData.img_obj.src}" alt="${assetData.asset_name}" />
+            <p>Initially Issued: <strong>${assetData.quantity}</strong></p>
+        `;
+    }
+}
+
+// Update tooltip position on the screen
+const updateTooltipPosition = (tooltip, event) => {
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+    const tooltipWidth = tooltip.offsetWidth;
+    const tooltipHeight = tooltip.offsetHeight;
+    const viewportWidth = window.innerWidth;
+
+    tooltip.style.top = mouseY < 400 ? `${mouseY + 10}px` : `${mouseY - (tooltipHeight + 10)}px`;
+    tooltip.style.left = mouseX + tooltipWidth / 2 > viewportWidth ? `${viewportWidth - tooltipWidth - 10}px` 
+                        : mouseX - tooltipWidth / 2 < 0 ? '10px'
+                        : `${mouseX - (tooltipWidth / 2)}px`;
+}
+
+// Tooltip Mouse Handler
 const linkMouseHandler = (tooltip, link, event, isClick = false) => {
     if (!nftData) {
         tooltip.innerHTML = 'Data is not loaded yet.';
         return;
     }
 
-    const assetName = link.dataset.assetName;
-    const assetData = nftData.find(item => item.asset_name === assetName);
-
-    if (!assetData) {
-        tooltip.innerHTML = 'No data found for this asset.';
-    } else {
-        tooltip.innerHTML = `
-            <h3>${assetData.asset_name}</h3>
-            <p>Series <strong>${assetData.series}</strong>&nbsp; Card <strong>${assetData.order}</strong></p>
-            `;
-        tooltip.appendChild(assetData.img_obj);
-        tooltip.innerHTML += `
-            <p>Initially Issued: <strong>${assetData.quantity}</strong></p>
-        `;
-    }
-
     if (isClick) {
-        // Prevent the document's touchstart event from getting triggered
         event.stopPropagation();
 
         if (activeTooltip === tooltip) {
@@ -55,8 +69,7 @@ const linkMouseHandler = (tooltip, link, event, isClick = false) => {
             activeTooltip = null;
         } else {
             if (documentTouchTriggered) {
-                // Do not reopen tooltip if documentTouchTriggered is true
-                documentTouchTriggered = false; // reset flag
+                documentTouchTriggered = false;
             } else {
                 tooltip.style.display = 'block';
                 if (activeTooltip) activeTooltip.style.display = 'none';
@@ -71,47 +84,23 @@ const linkMouseHandler = (tooltip, link, event, isClick = false) => {
     updateTooltipPosition(tooltip, event);
 }
 
-const updateTooltipPosition = (tooltip, event) => {
-    // Positioning logic
-    let mouseX = event.clientX;
-    let mouseY = event.clientY;
-    const tooltipWidth = tooltip.offsetWidth;
-    const tooltipHeight = tooltip.offsetHeight;
-    const viewportWidth = window.innerWidth;
-
-    // If tooltip overflows to the top, place below cursor
-    if (mouseY < 400) {
-        tooltip.style.top = `${mouseY + 10}px`;
-    } else {
-        tooltip.style.top = `${mouseY - (tooltipHeight + 10)}px`;
-    }
-
-    // If tooltip overflows to the sides, place at edge of viewport
-    if (mouseX + tooltipWidth / 2 > viewportWidth) {
-        tooltip.style.left = `${viewportWidth - tooltipWidth - 10}px`; // 10px padding from right
-    }
-    else if (mouseX - tooltipWidth / 2 < 0) {
-        tooltip.style.left = '10px'; // 10px padding from left
-    }
-
-    // Normal centered positioning
-    else {
-        tooltip.style.left = `${mouseX - (tooltipWidth / 2)}px`;
-    }
+// Create tooltip element
+const createTooltipElement = (assetName) => {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'rs-tooltip';
+    document.body.appendChild(tooltip);
+    const assetData = nftData.find(item => item.asset_name === assetName);
+    tooltip.innerHTML = generateTooltipHTML(assetData);
+    return tooltip;
 }
 
+// Initialize tooltips for all the links on the page
 const initTooltips = () => {
     const links = document.querySelectorAll('.rs-link');
-    if (!links) {
-        return;
-    }
-
     const isTouchDevice = 'ontouchstart' in document.documentElement;
 
     links.forEach((link) => {
-        const tooltip = document.createElement('div');
-        tooltip.className = 'rs-tooltip';
-        document.body.appendChild(tooltip);
+        const tooltip = createTooltipElement(link.dataset.assetName);
 
         if (isTouchDevice) {
             link.addEventListener('click', (event) => {
@@ -144,7 +133,8 @@ const initTooltips = () => {
     }
 }
 
-document.addEventListener('DOMContentLoaded', async (event) => {
+// When the document is loaded, fetch data and initialize tooltips
+document.addEventListener('DOMContentLoaded', async () => {
     await fetchData();
     initTooltips();
 });
